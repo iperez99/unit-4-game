161cad2c3ddb9a65b650cc32ebeb7d6f0bd24917
Qbisst <ismaelaperez@outlook.com>
2019-02-10 21:22:51 -0500

    almost done with getting the score to work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,25 +36,96 @@ var playerScore = 0;
 var wins = 0;
 var losses = 0;
 
+//End of Global Variables//
+// ========================================================================================
+
+//Functions//
+
+//this Function creates the random numbers for the game. I found this on:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random// //
+
+var getRandom = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var startGame = function () {
+    //resets the score to reach//
+    var reachScore = 0;
+
+    //creates a new score to reach (between 21 and 123)//
+    reachScore = getRandom(21, 123);
+
+    //This creates different values for each crystal//
+    crystal.diamond.value = getRandom(1, 13);
+    crystal.emerald.value = getRandom(1, 13);
+    crystal.ruby.value = getRandom(1, 13);
+    crystal.sapphire.value = getRandom(1, 13);
+
+    console.log("target score " + reachScore);
+    console.log("Diamond: " + crystal.diamond.value + "| Emerald: " + crystal.emerald.value + " | Ruby: " + crystal.ruby.value + " | Sapphire: " + crystal.sapphire.value);
+
+    //JQUERY - Injects to HTML file the player score and the score to reach//
+    $("#reachScore").html(reachScore);
+    $("#playerScore").html(playerScore);
+}
+
+//when player clicks on the crystal a number value gets attached to the crystal and displays the score totals//
+
+var addValue = function(crystal){
+
+    //this changes the player score//
+    playerScore = playerScore + crystal.value;
+
+    //this JQUERY code changes the value of the Player Score in HTML//
+    $("#playerScore").html(playerScore);
+
+    //Call of the winCheck function//
+    winCheck();
+
+    console.log("player score" + playerScore);
+
+}
+ // this checks if the player has won or lost//
+var winCheck  = function () {
+
+    if(playerScore > reachScore){
+        
+        alert("Sorry, please try again :(");
+
+
+        console.log("loss")
+    }
+    
+    else if(playerScore === reachScore) {
+        alert("You are awesome, you won!")
+
+        console.log("win")
+    }
+}
+
 //JQUERY- Wiring the crystal pictures to a click event//
 
-$("#diamond").click(function(){
-    alert("test");
+$("#diamond").click(function () {
+    addValue(crystal.diamond);
 })
-$("#emerald").click(function(){
-    alert("test");
+$("#emerald").click(function () {
+    addValue(crystal.emerald);
 })
-$("#ruby").click(function(){
-    alert("test");
+$("#ruby").click(function () {
+    addValue(crystal.ruby);
 })
-$("#sapphire").click(function(){
-    alert("test");
+$("#sapphire").click(function () {
+    addValue(crystal.sapphire);
 })
-
-
 
 
 // JQUERY Wiring the instructions button to a slide toggle function//
-$("#btn").click(function() {
+$("#btn").click(function () {
     $(".instructions").slideToggle();
 });
+
+//Function call to start the game//
+startGame();
+
+
+
